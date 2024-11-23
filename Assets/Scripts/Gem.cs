5cf4a444a62696903e7610c032e4fd3f4bef0d8d
omerbsagir <omerbesagir@gmail.com>
2024-11-23 22:17:12 +0300

    vertical , hoizontal , combo prefableri eklendş

--- Assets/Scripts/Gem.cs
+++ Assets/Scripts/Gem.cs
@@ -17,7 +17,7 @@ public class Gem : MonoBehaviour
 
     private Gem otherGem;
 
-    public enum GemType { a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,bomb,glass,grass,wood , hidden}
+    public enum GemType { a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,bomb,glass,grass,wood , hidden,horizontal,vertical,combo}
     public GemType type;
     public string letterValue;
 
